4a7009988cd404821b4de9982b5895ffaf4f4052
Anna <annabaumann@laposte.net>
2019-01-21 23:52:44 +0100

    add routes for project

--- src/router.js
+++ src/router.js
@@ -25,6 +25,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/project/:name",
+      name: "project",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "project" */ "./views/Project.vue")
     }
   ]
 });
---
+++ src/views/Project.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1>{{this.$route.params.name}}</h1>
+  </div>
+</template>
