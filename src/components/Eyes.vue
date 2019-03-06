1bf0a960cd9f1cbdd0f1f3c3c408a651f9077f40
Anna <annabaumann@laposte.net>
2019-03-06 16:48:15 +0100

    transition

--- src/components/Eyes.vue
+++ src/components/Eyes.vue
@@ -17,9 +17,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-circle {
-    transition: transform ease-in-out 0.2s ;
-}
+
 </style>
 
 
